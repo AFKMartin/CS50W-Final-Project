d824c877e8729eb79f4d7e510e09bc1ee9e97b40
AFKMartin <javiermarace@protonmail.com>
2025-12-14 23:43:47 +0000

    urls wired

--- explorer/urls.py
+++ explorer/urls.py
@@ -1,8 +1,7 @@
-# demo/urls.py
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("explorer.urls")),
+    path("", views.index, name="index"),
+    path("register/", views.register, name="register"),
 ]
--- explorer/views.py
+++ explorer/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# explorer/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
 
-# Create your views here.
+def index(request):
+    return render(request, "explorer/index.html")
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect("index")
+    else:
+        form = RegisterForm()
+    return render(request, "explorer/register.html", {"form": form})
